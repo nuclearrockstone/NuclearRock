a8116660fe354c3fd66daaf57e17c3866869c3e1
nulearrockstone <nuclearrockstone@gmail.com>
2025-01-03 15:34:42 +0800

    Add Dropdown

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,10 +96,21 @@ const config = {
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            type: 'dropdown',
             position: 'left',
             label: 'Archive',
+            items:[
+              {
+                type: 'docSidebar',
+                sidebarId: 'tutorialSidebar',
+                label: 'Tutorial',
+              },
+              {
+                type: 'docSidebar',
+                sidebarId: 'pythonSidebar',
+                label: 'Python',
+              },
+            ]
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           /* {
