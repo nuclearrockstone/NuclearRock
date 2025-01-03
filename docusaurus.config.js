21c43662e62fb3cf69d1db8a87682bcc4b1e301a
nulearrockstone <nuclearrockstone@gmail.com>
2025-01-03 20:50:02 +0800

    Debug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,11 +8,6 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
-const base_url = String(process.env.BASE_URL);
-
-console.log("Type: " + typeof process.env.BASE_URL);  // 打印变量类型
-console.log("Value: " + base_url); 
-
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
@@ -25,7 +20,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl:base_url,
+  baseUrl:process.env.BASE_URL || '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
