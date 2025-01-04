cef32052c6089f89af59d0ab4d5bdaf223f1bc55
nulearrockstone <nuclearrockstone@gmail.com>
2025-01-04 14:20:26 +0800

    Update Blog

---
+++ blog/2025-01-04-My-First-Blog.md
@@ -0,0 +1,11 @@
+---
+slug: 第一篇博文
+title: 第一篇博文
+authors: nuclearrockstone
+tags: [hello, docusaurus]
+---
+这是我的第一篇博文
+<!-- truncate -->
+目前的计划
+- [ ] 完善多库龙教程
+- [ ] 尝试在Notion和Docusaurus之间同步内容
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,9 +67,6 @@ const config = {
             xslt: true,
           },
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
